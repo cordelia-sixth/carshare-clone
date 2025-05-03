b2f471942a22dd69cdda433d3d717f84c9d1cb9e
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-05-03 16:47:28 +0900

    change footer design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,11 +5,15 @@ import Link from 'next/link';
  */
 export const Footer = () => {
   return (
-    <footer className="">
-      <div className="m-auto max-w-[960px] space-y-3 py-3">
-        <h1>CAR-SHARE</h1>
-        <p>CAR-SHAREは良さげなカーシェアリングサービスです。</p>
-        <ul className="flex gap-2">
+    <footer className="border-t-1 border-gray-300">
+      <div className="m-auto max-w-[960px] space-y-4 px-4 py-7">
+        <div>
+          <h1 className="text-xl">CAR-SHARE</h1>
+          <p className="text-sm text-gray-500">
+            CAR-SHAREは良さげなカーシェアリングサービスです。
+          </p>
+        </div>
+        <ul className="flex gap-2 text-xs text-blue-500">
           <li>
             <Link href="#">利用規約</Link>
           </li>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,10 +7,10 @@ import { PCMenu } from './menu/pc-menu';
 export const Header = () => {
   return (
     <header className="border-b-1 border-gray-300">
-      <div className="m-auto flex max-w-[960px] justify-between p-4">
+      <div className="m-auto flex max-w-[960px] justify-between p-3">
         <div className="flex items-center gap-3">
           <Menu />
-          <h1>CAR-SHARE</h1>
+          <h1 className="text-xl">CAR-SHARE</h1>
         </div>
         <PCMenu />
       </div>
